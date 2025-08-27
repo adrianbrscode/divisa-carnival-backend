8818ff504d2527126ee15fe7548c40a2d453ce37
AdrianBRScode <penalozaadrian02@gmail.com>
2025-08-27 05:26:58 -0500

    Crear la estructura básica del proyecto Spring Boot
    Se creó la estructura inicial del proyecto, incluyendo paquetes `client`, `config`, `controller`, `dto`, `enums`, `service`, `model`.
    También se añadieron archivos `.gitkeep` para mantener la estructura de directorios vacíos.

---
+++ src/main/java/com/adrianbrscode/divisa/DivisaCarnivalApplication.java
@@ -0,0 +1,13 @@
+package com.adrianbrscode.divisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisaCarnivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisaCarnivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrianbrscode/divisa/DivisaCarnivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianbrscode.divisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisaCarnivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
